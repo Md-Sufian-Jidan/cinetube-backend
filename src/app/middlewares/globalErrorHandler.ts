725389d37e344720d7e36667cee001a539ddcd3b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-03-17 22:50:12 +0600

    Added global error handler and zod error handler

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,32 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import status from "http-status";
+import { ZodError } from "zod";
+import handleZodError from "../errors/handleZodError";
+import { TErrorSources } from "../interfaces/error";
+
+const globalErrorHandler: ErrorRequestHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    let statusCode = status.INTERNAL_SERVER_ERROR as number;
+    let message = 'Something went wrong';
+    let errorSources: TErrorSources = [];
+    let stack = err.stack;
+
+    if (err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = [...simplifiedError.errorSources];
+        stack = err.stack;
+    };
+
+    const errorResponse = {
+        success: false,
+        statusCode,
+        message,
+        errorSources,
+        stack,
+    };
+
+    res.status(statusCode).json(errorResponse);
+};
+
+export default globalErrorHandler;
